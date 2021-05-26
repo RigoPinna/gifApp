e5e4e9e3c6373732f52de5591a32ff9baf2b1521
Rigoberto Cepeda <riguxc@gmail.com>
2021-05-26 00:23:39 -0500

    update google analytics

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -2,11 +2,16 @@ import React from 'react'
 import { Redirect, Switch, BrowserRouter, Route } from 'react-router-dom';
 import GifExperApp from '../GifExpertApp';
 import { GifFavorite } from '../GifFavorite';
-
+import ReactGA from 'react-ga';
 
 
 export const AppRouter = () => {
-    
+    ReactGA.initialize('G-YBM4R832NX'); // Aqui pones tu identificador de cuenta de Google Analytics
+    ReactGA.event({
+        category: 'Usuario',
+        action: 'Cuenta creada'
+    });
+    history.listen(location => ReactGA.pageview(location.pathname));
 
     return (
              <BrowserRouter basename={process.env.PUBLIC_URL}>
